66184784fda3959f4bd99cbd0b088e6a33996ed3
fage2022 <107664592+fage2022@users.noreply.github.com>
2026-03-13 12:14:19 +0800

    8

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import cron from "./cron.js";
 import http from "./http.js";
 import ws from "./ws.js";
 cron();
-
+ws();
 Deno.serve(async(req) => {
     if (req.headers.get("upgrade") != "websocket") {
       return await http(req);
