b01a467d85ca1d6eabefb3d0769f70978d58de0f
fage2022 <107664592+fage2022@users.noreply.github.com>
2026-03-13 10:25:23 +0800

    改成80端口，关闭WS
    Comment out WebSocket server initialization and related code.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from 'express'
-import { WebSocketServer } from 'ws';
+// import { WebSocketServer } from 'ws';
 
-const wss = new WebSocketServer({ port: 8080 });
+
 const app = express()
 
 app.get('/', (req, res) => {
@@ -9,21 +9,21 @@ app.get('/', (req, res) => {
 })
 
 app.listen(80, () => {
-    console.log('Server is running on http://localhost:3000')
+    console.log('web运行在80端口')
 })
 
 
 
 
+// const wss = new WebSocketServer({ port: 8080 });
+// wss.on('connection', function connection(ws) {
+//     ws.on('error', console.error);
 
-wss.on('connection', function connection(ws) {
-    ws.on('error', console.error);
+//     ws.on('message', function message(data) {
+//         console.log('received: %s', data);
+//     });
 
-    ws.on('message', function message(data) {
-        console.log('received: %s', data);
-    });
+//     ws.send('something');
+// });
 
-    ws.send('something');
-});
-
-console.log('WebSocket server is running on ws://localhost:8080');
+// console.log('WebSocket server is running on ws://localhost:8080');
