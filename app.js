e4892b3f976720906b4358519adb156bb6b620a0
fage <85582508@qq.com>
2026-03-13 09:40:37 +0800

    init

---
+++ app.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 8080 });
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000')
+})
+
+
+
+
+
+wss.on('connection', function connection(ws) {
+    ws.on('error', console.error);
+
+    ws.on('message', function message(data) {
+        console.log('received: %s', data);
+    });
+
+    ws.send('something');
+});
+
+console.log('WebSocket server is running on ws://localhost:8080');
